667462045548864f2f63c18c5be2e6209cd3613b
Mansi Khemka <mk4282@columbia.edu>
2020-04-20 22:17:17 -0400

    Updating Clusters

---
+++ src/FileExplorer/header/directoryIterator.h
@@ -0,0 +1,18 @@
+//
+// Created by Anmol Chugh on 4/10/20.
+//
+
+#ifndef FACETY_IMAGEITERATOR_H
+#define FACETY_IMAGEITERATOR_H
+
+#include <stdio.h>
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> list_all_images(const std::filesystem::path& path);
+
+#endif //FACETY_IMAGEITERATOR_H
